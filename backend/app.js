22ccf33610861082a14f58f9afaeb10048f6200b
Eduardomnts31 <corhers234@gmail.com>
2025-06-09 23:39:36 -0600

    agregue una funcion de obtener la ip

--- backend/app.js
+++ backend/app.js
@@ -6,22 +6,24 @@ import genRoutes from './rutas/rutasGeneracion.js';
 import usuariosRoutes from './rutas/rutasUsuarios.js';
 import db from "./database/db.js";
 import lgInUsr from './rutas/rutasLogIn.js';
-
-
-try {
-  
-} catch (error) {
-  
-}
+import ipRouter from './rutas/rutasIp.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+
+
 
 app.use('/api/roles', rolRoutes);//api crud roles
 app.use('/api/programas', programasRoutes);//api crud programas
 app.use('/api/generaciones', genRoutes);//api crud generaciones
 app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/login', lgInUsr);
+
+app.use('/api/ip', ipRouter);
+
+
 //app.get("/api/test", (req, res) => {
 //  res.json({message:"Servidor funcionando"});
 //});
@@ -34,9 +36,9 @@ async function conectarDB() {
     console.log("Hubo un error al conectarse a la BBDD, inténtalo nuevamente", error);
   }
 }
-
 conectarDB();
 
-app.listen(8000, '0.0.0.0', ()=>{
+app.listen(8000, '0.0.0.0', (req, res)=>{
     console.log("SERVIDOR CORRIENDO DEL BACKEND")
+    
 })
--- frontend/src/Api/ApiPrefix.tsx
+++ frontend/src/Api/ApiPrefix.tsx
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const currentHost = window.location.hostname; //obtiene la url que tenemos en el navegador, en este caso seria la ip del nabegador
+const res = await fetch(`http://${currentHost}:8000/api/ip`);//la colocamos para hacer un get en la api que tengo en en back
+const data = await res.json();//si se cumple obtengo la ipque esta en mi controlador 
+
 const api = axios.create({
-    baseURL: 'http://192.168.101.120:8000/api', 
+    
+    baseURL: `http://${data.ip}:8000/api`, //y la colocamos en la BASE URL para hacer la peticion de crear
     headers: {
         'Content-Type': 'application/json',
     },
